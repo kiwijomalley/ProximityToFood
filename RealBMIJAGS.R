0c28962ed72d2182f4951088d5f478cd05b46e1a
kiwijomalley <kiwijomalley@hotmail.com>
2018-05-17 10:29:55 -0400

    Add files via upload

--- RealBMIJAGS.R
+++ RealBMIJAGS.R
@@ -1,24 +1,52 @@
 ## R wrapper code to estimate distance-to-food model ##
 
+## TO DO: If there is further push-back need to determine if residual correlation can be more accurately estimated.
+##        Is the problem due to the way the model is specified in JAGS?
+##        If can't resolve, might be best to drop error variance and serial correlation parameters from the simulation figure and results!
+
 #Set-up: Load in libraries
 library(rjags)
 coda.options(combine.plots=TRUE,combine.stats=TRUE)
 
 #Specify filename of JAGS code for Bayesian analysis and data in and output directories
-rsource="Enter directory where code is stored" #If run on a Linux server
+rsource="/Volumes/STORAGE/JOMalley/Dartmouth/Biostatistics/DistanceToFood/Code" #If run on a Linux server
 setwd(rsource)
-datdir="../Data/" #Sub-directory to store data
-outdir="../Output/" #Sub-directory to store output
+datdir="../Data/" #Directory to store data
+outdir="../Output/" #Directory to store output
 
 #Specify code and initial values to use
-JAGScode<-"RealBMICodeIntIW.bug"
-IC<-list(be=c(30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), isigma=0.25, u=0.5) #Initial values
+IW=1
+subtype=1
+if (IW==1) {
+ if (subtype==1) {
+  JAGScode<-"RealBMICodeIntIW.bug"
+  #JAGScode<-"RealBMICodeIntIWlog.bug"
+ } else if (subtype==2) {
+  JAGScode<-"RealBMICodeIntArea0.bug"
+ } else if (subtype==3) {
+  JAGScode<-"RealBMICodeIntRSlope0.bug"
+ } else {
+  JAGScode<-"RealBMICodeIntSerial0.bug"
+ } 
+ IC<-list(be=c(30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), isigma=0.25, u=0.5) #Initial values
+} else {
+ JAGScode<-"RealBMICodeIntProdNorm.bug"
+ IC<-list(be=c(30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), isigma=0.25, ipntauarea=c(1, 0.1), ipntaurand=c(1, 0.1), psirand=0, psiarea=0, u=0.5) #Initial values	
+}
 
 #Read in data
-BMIdata<-read.table(paste(datdir,"simdata.txt",sep=""),sep=" ",col.names=c("ID", "wave", "Omni", "Tobs", "TractHome", "TractWork", "BMI", "yob", "smokes", "male", "married", "educ", "tractpov", "etractpov", "unemploy", "DistHome", "DistWork", "DriveDist"), skip=1) #Read in data for analysis
+datatype=1
+if (datatype==1) {
+ #BMIdata<-read.table(paste(datdir,"simdata.txt",sep=""),sep=" ",col.names=c("ID", "wave", "Omni", "Tobs", "TractHome", "TractWork", "BMI", "yob", "smokes", "male", "married", "educ", "tractpov", "etractpov", "unemploy", "DistHome", "DistWork", "DriveDist"), skip=1) #Read in data for analysis
+ BMIdata<-read.table(paste(datdir,"datawbugs4town.txt",sep=""),sep=" ",col.names=c("ID", "wave", "Omni", "Tobs", "TractHome", "TractWork", "BMI", "yob", "smokes", "male", "married", "educ", "tractpov", "etractpov", "unemploy", "DistHome", "DistWork", "DriveDist"), skip=1) #Read in data for analysis
+} else if (datatype==2) {
+ BMIdata<-read.table(paste(datdir,"datawbugs14town.txt",sep=""),sep=" ",col.names=c("ID", "wave", "Omni", "Tobs", "TractHome", "TractWork", "BMI", "yob", "smokes", "male", "married", "educ", "tractpov", "etractpov", "unemploy", "DistHome", "DistWork", "DriveDist"), skip=1) #Read in data for analysis
+} else {
+ BMIdata<-read.table(paste(datdir,"datawbugs4towncf.txt",sep=""),sep=" ",col.names=c("ID", "wave", "Omni", "Tobs", "TractHome", "TractWork", "BMI", "yob", "smokes", "male", "married", "educ", "tractpov", "etractpov", "unemploy", "DistHome", "DistWork", "DriveDist"), skip=1) #Read in data for analysis
+}
 
 #Define parameters and operating characteristics of MCMC procedure
-parameters = c("be", "sigma", "rho", "taurand", "tauarea", "corrand", "corarea")    # The parameter(s) to be monitored.
+parameters = c("be", "sigma", "rho", "taurand", "tauarea", "corrand", "corarea", "Rsquare")    # The parameter(s) to be monitored.
 adaptSteps = 500              # Number of steps to "tune" the samplers.
 burnInSteps = 20000           # Number of steps to "burn-in" the samplers.
 nChains = 3                   # Number of chains to run.
@@ -62,7 +90,7 @@ plot(sigmaSample[1:nIter],type="l",main="MCMC Chain for error variance",xlab="it
 plot(tau1Sample[1:nIter],type="l",main="MCMC Chain for home variance",xlab="iteration",ylab="estimate")
 plot(corrandSample[1:nIter],type="l",main="MCMC Chain for individual RE correlation",xlab="iteration",ylab="estimate")
 plot(corareaSample[1:nIter],type="l",main="MCMC Chain for area RE correlation",xlab="iteration",ylab="estimate")
-dev.copy2eps(file=paste(outdir,"TraceSeqIW4sim.eps",sep=""), width=6, height=6, horizontal=FALSE) #to file
+dev.copy2eps(file=paste(outdir,"TraceSeqIW4.eps",sep=""), width=6, height=6, horizontal=FALSE) #to file
 
 #Trace plots of long-run averages
 par(mfrow=c(1,1), srt=0, mai=c(0.6, 0.6, 0.4, 0.2), mgp=c(2,1,0))
@@ -73,44 +101,62 @@ for (i in 2:nIter) {
  trpSample[i,3]=((i-1)*trpSample[2*nIter+i-1]+corareaSample[2*nIter+i])/i
 }
 mn=mean(corareaSample); sd=sqrt(var(corareaSample))
-plot(trpSample[,1],type='l',ylim=c(mn-sd,mn+sd),xlim=c(0,nIter),
+plot(trpSample[,1],type='l',ylim=c(mn-0.25*sd,mn+0.25*sd),xlim=c(0,nIter),
  main='Trace plot of posterior mean for 3 chains',
  xlab='Draw',
  ylab='Mean')
 lines(trpSample[,2],type='l',col='red')
 lines(trpSample[,3],type='l',col='green')
-dev.copy2eps(file=paste(outdir,"TracePlotIW4sim.eps",sep=""), width=6, height=6, horizontal=FALSE) #to file
+dev.copy2eps(file=paste(outdir,"TracePlotIW4.eps",sep=""), width=6, height=6, horizontal=FALSE) #to file
 
 #Autocorrelation plots
 par(mfrow=c(1,1), srt=0, mai=c(0.6, 0.6, 0.4, 0.2), mgp=c(2,1,0))
 autocorr.plot(corareaSample[1:nIter],main="MCMC Chain for p",auto.layout=FALSE)
-dev.copy2eps(file=paste(outdir,"AutoCorrPlotIW4sim.eps",sep=""), width=6, height=6, horizontal=FALSE) #to file
+dev.copy2eps(file=paste(outdir,"AutoCorrPlotIW4.eps",sep=""), width=6, height=6, horizontal=FALSE) #to file
 
+#Gelman-Rubin MCMC convergence statistic
+MCMCdiag=gelman.diag(codaSamples,confidence=0.95,autoburnin=FALSE,multivariate=FALSE)$psrf
 
-## Generate statistical inferences and outputs ##
+#Residual analysis
+rsquare=mcmcChain[,"Rsquare"]
+print(summary(rsquare))
+
+## Generate statistical inferences ##
 
 #Plots of drawn values
 par(mfrow=c(3,2), srt=0, mai=c(0.6, 0.6, 0.4, 0.2), mgp=c(2,1,0))
-plot(density(mcmcChain[,"be[1]"]),xlab="",ylab="Density",
- main="Intercept")
-plot(density(mcmcChain[,"be[2]"]),xlab="",ylab="Density",
- main="Wave 2 effect")
-plot(density(mcmcChain[,"be[3]"]),xlab="",ylab="Density",
- main="Wave 3 effect")
-plot(density(mcmcChain[,"be[4]"]),xlab="",ylab="Density",
- main="Wave 4 effect")
+plot(density(mcmcChain[,"be[18]"]),xlab="",ylab="Density",
+ main="Home distance to FF effect for females")
+plot(density(mcmcChain[,"be[21]"]),xlab="",ylab="Density",
+ main="Work distance to FF effect for males")
+plot(density(mcmcChain[,"tauarea[1,1]"]),xlab="",ylab="Density",
+ main="Residential Std. Dev.")
+plot(density(mcmcChain[,"tauarea[2,2]"]),xlab="",ylab="Density",
+ main="Workplace Std. Dev.")
 plot(density(mcmcChain[,"corrand"]),xlab="",ylab="Density",
- main="Correlation for individual random effects")
+ main="Correlation of individual random effects")
 plot(density(mcmcChain[,"corarea"]),xlab="",ylab="Density",
- main="Correlation for area random effects")
-dev.copy2eps(file=paste(outdir,"FixedIW4sim.eps",sep=""), width=6, height=6, horizontal=FALSE) #to file
+ main="Correlation of area random effects")
+dev.copy2eps(file=paste(outdir,"FixedSerialIW4.eps",sep=""), width=6, height=6, horizontal=FALSE) #to file
 
 #Summary statistics of draws (the types of things you might put in a paper)
 mn=apply(mcmcChain,2,mean)
 stdev=sqrt(apply(mcmcChain,2,var))
 lowl=apply(mcmcChain,2,quantile,0.025)
 uppl=apply(mcmcChain,2,quantile,0.975)
-sumdata=cbind(mn,stdev,lowl,uppl)
-write.table(t(sumdata),file=paste(outdir,"BayesSummaryStatisticsIW4sim.txt",sep=""))
+pg0=apply(mcmcChain,2,function(x) mean(x>0))
+sumdata=cbind(mn,stdev,lowl,uppl,pg0)
+write.table(t(sumdata),file=paste(outdir,"BayesSummaryStatisticsIW4.txt",sep=""))
+
+#Generate estimates for joint hypothesis test of female home and male workplace proximity
+prHypW=mean(mcmcChain[,"be[18]"]<0)
+prHypM=mean(mcmcChain[,"be[21]"]<0)
+prJointHyp=mean(mcmcChain[,"be[18]"]<0 & mcmcChain[,"be[21]"]<0)
+print(c(prHypW,prHypM,prJointHyp))
+
+#Posterior correlation between error variance and serial correlation
+cor(mcmcChain[,"sigma"],mcmcChain[,"rho"])
+
+
 
  
